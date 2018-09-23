12ae4fd03a1d8550b8f46bb177c1d674bfc590ad
Ikraduya <ikraduya@gmail.com>
2018-09-24 05:31:47 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    Matrix Mat;
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+    }
+
+}
