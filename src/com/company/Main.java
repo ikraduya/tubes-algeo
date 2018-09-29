06bdebef979d79eb30e32cd57170cb70fdd8dd4b
Azhar Abdurrasyid <39158806+Razor012@users.noreply.github.com>
2018-09-29 07:11:14 +0700

    Commenting dan Method Main
    Udah lulus compile

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,80 +1,126 @@
 package com.company;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
     Matrix Mat;
-    static Double[][] mtrxHasil = new Double[100][100];
-    static int NBrsEff = 1;
-    static int NKolEff = 1;
-    static Scanner keyboard = new Scanner(System.in);
+    static Double[][] mtrxHasil = new Double[100][100]; //Matriks untuk menyimpan data SPL
+    static int NBrsEff = 1; //Jumlah baris mtrxHasil. Asumsi input tidak pernah kosong
+    static int NKolEff = 1; //Jumlah kolom mtrxHasil. Asumsi input tidak pernah kosong
+    static Scanner keyboard = new Scanner(System.in); //Variabel untuk menampung inputan dari keyboard
+    static boolean selesai = false; //Variavel untuk menghentikan/melanjutkan program
+    static int pilihanProblem; //Variabel untuk menampung pilihan user
+    static int pilihanMetode; //Variabel untuk menampung pilihan user
+    static int pilihanInpt; //Variabel untuk menampung pilihan user
 	
     public static void main(String[] args) throws IOException {
-	// write your code here
-        System.out.println("hello");
+	while (!selesai){
+	  System.out.println("MENU");
+	  System.out.println("1. Sistem Persamaan Linear");
+	  System.out.println("2. Interpolasi Problem");
+	  System.out.println("3. Selesai");
+	  System.out.print("Pilihan : ");
+	  pilihanProblem = keyboard.nextInt(); //Memasukkan pilihan problem
+
+	  if (pilihanProblem == 3){
+	    selesai = true; //Menghentikan program
+	  }
+
+	  if (!selesai){
+	    System.out.println("Metode Input");
+            System.out.println("1. File txt eksternal");
+	    System.out.println("2. Matriks masukkan");
+	    System.out.print("Pilihan : ");
+	    pilihanInpt = keyboard.nextInt(); //Memasukkan pilihan metode input
+
+	    if (pilihanInpt == 1){
+	      BacaFile(mtrxHasil,NBrsEff,NKolEff);
+	    }
+	    else{
+	      BacaMatriks(mtrxHasil,NBrsEff,NKolEff);
+	    }
+
+	    System.out.println("Metode Penyelesaian");
+	    System.out.println("1. Eleminasi Gauss");
+	    System.out.println("2. Eleminasi GaussJordan");
+	    System.out.print("Pilihan : ");
+	    pilihanMetode = keyboard.nextInt(); //Memasukkan pilihan metode penyelesaian
+	 }
+       }
     }
 	
-    static void BacaMatriks (Double[][] mtrxHasil, int NBrsEff, int NKolEff) {
+    static void BacaMatriks (Double[][] mtrxHasil, int NBrsEff, int NKolEff)
+    //I.S mtrxHasil, NBrsEff, dan NBrsKol sembarang.
+    //F.S mtrxHasil, NBrsEff, dan NBrsKol terdefinisi sesuai inputan user
+    {
      System.out.print("Masukkan jumlah baris : ");
-     NBrsEff = keyboard.nextInt();
+     NBrsEff = keyboard.nextInt(); //Memasukkan jumlah baris
      System.out.print("Masukkan jumlah kolom : ");
-     NKolEff = keyboard.nextInt();
+     NKolEff = keyboard.nextInt(); //Memasukkan jumlah kolom
      for (int i =1; i <= NBrsEff; i++){
        for (int j =1; j <= NKolEff; j++){
-         mtrxHasil[i][j] = keyboard.nextDouble();
+         mtrxHasil[i][j] = keyboard.nextDouble(); //Memasukkan data matriks
        }
      }
    }
 
-   static void BacaFile (Double[][] mtrxHasil) throws IOException {
-     FileReader in = null;
-     String[][] isiFile = new String[100][100];
-     int Kol = 1;
-     int Brs = 1;
-     String NamaFile;
+   static void BacaFile (Double[][] mtrxHasil, int NBrsEff, int NKolEff) throws IOException
+   //I.S mtrxHasil, NBrsEff, dan NBrsKol sembarang
+   //F.S mtrxHasil, NBrsEff, dan NBrsKol terdefinisi sesuai isi file 	   
+   {
+     FileReader in = null; 
+     String[][] isiFile = new String[100][100]; //Variabel untuk menampung isi file
+     int Kol = 1; //Inisiasi jumlah kolom minimal. Asumsi file tidak pernah kosong
+     int Brs = 1; //Inisiasi jumlah baris minimal. Asumsi file tidak pernah kosong
+     String NamaFile; //Variabel untuk menampung nama file
      System.out.print("Masukkan NamaFile : ");
-     NamaFile = keyboard.next();
+     NamaFile = keyboard.next(); //Memasukkan nama file
      try {
-        in = new FileReader(NamaFile);
-        int cc;
+        in = new FileReader(NamaFile); //Mempersiapkan variabel untuk pembacaan file
+        int cc; //Variabel untuk menampung isi file per karakter
         isiFile[Brs][Kol] = "";
-        while ((cc =  in.read()) != -1) {
-           if (cc == 13) {
+        while ((cc =  in.read()) != -1) { //Looping selama isi file masih ada
+           if (cc == 13) { //cc == 13 menandakan karakter terakhir dari baris ke-x dari isi file
              Brs++;
              NKolEff++;
            }
-           else if (cc == 32) {
+           else if (cc == 32) { //cc == 32 menandakan cc adalah spasi
              Kol++;
              NKolEff++;
              isiFile[Brs][Kol] = "";
            }
-           else if (cc == 10){
+           else if (cc == 10){ //cc == 10 berarti pembacaan file berpindah baris
              Kol = 1;
              isiFile[Brs][Kol]= "";
            }
            else {
-             isiFile[Brs][Kol] += (char) cc;
+             isiFile[Brs][Kol] += (char) cc; //Mengisi matriks string dengan cc
            }
         }
      }
      finally {
-        if (in != null) {
+        if (in != null) { //Menutup file jika file sudah selesai dibaca
            in.close();
         }
      }
-     NBrsEff = Brs -1;
-     NKolEff = NKolEff / NBrsEff;
+     NBrsEff = Brs -1; //Jumlah baris matriks
+     NKolEff = NKolEff / NBrsEff; //jumlah kolom matriks
      for (int i = 1; i <= NBrsEff; i += 1){
        for (int j = 1; j <= NKolEff; j += 1){
-        mtrxHasil[i][j] = Double.parseDouble(isiFile[i][j]);
+        mtrxHasil[i][j] = Double.parseDouble(isiFile[i][j]); //Mengisi matriks double dengan matriks string yang sudah dikonversi
        }
      }
    }
 
-   static void interpolasi(Double[][] mtrxHasil, int NBrsEff, int NKolEff){
-    Double[][] mtrxInter = new Double[100][100];
+   static void interpolasi(Double[][] mtrxHasil, int NBrsEff, int NKolEff)
+   //I.S mtrsHasil, NBrsEff, NKolEff terdefinisi
+   //F.S mengeluarkan hasil interpolasi
+   {
+    Double[][] mtrxInter = new Double[100][100]; //Variabel untuk menyimpan matriks augmented
     for (int i =1; i <= NBrsEff; i++){
       for (int j =1; j <= NBrsEff; j++){
-        mtrxInter[i][j] = Math.pow(mtrxHasil[i][1],j-1)
+        mtrxInter[i][j] = Math.pow(mtrxHasil[i][1],j-1); //Mengkonversi matriks yang masih dalam bentuk x,y menjadi matriks augmented
       }
       mtrxInter[i][NBrsEff+1] = mtrxHasil[i][2];
     }
